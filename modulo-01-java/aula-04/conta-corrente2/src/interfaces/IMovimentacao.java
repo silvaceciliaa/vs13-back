d13350601c32346dfc9aa8cc992d65425de42bda
silvaceciliaa <ceciliaalicesilva88@gmail.com>
2024-01-02 13:41:31 -0300

    fix: IMovimentacao deve conter apenas sacar, depositar e transferir

--- modulo-01-java/aula-04/conta-corrente2/src/interfaces/IMovimentacao.java
+++ modulo-01-java/aula-04/conta-corrente2/src/interfaces/IMovimentacao.java
@@ -1,13 +1,6 @@
 package interfaces;
 
 public interface IMovimentacao {
-
-    String getNumeroConta();
-    void setNumeroConta(String numeroConta);
-    String getAgencia();
-    void setAgencia(String agencia);
-    double getSaldo();
-    void setSaldo(double saldo);
     boolean sacar(double valor);
     boolean depositar(double valor);
     boolean transferir(IMovimentacao conta, double valor);
