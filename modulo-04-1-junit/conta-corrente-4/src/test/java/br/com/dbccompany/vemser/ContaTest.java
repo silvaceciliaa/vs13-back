27e64ba08e155f1f5cffc28fc4ea5672c6308c03
silvaceciliaa <ceciliaalicesilva88@gmail.com>
2024-02-15 00:36:31 -0300

    feat: task concluída

--- modulo-04-1-junit/conta-corrente-4/src/test/java/br/com/dbccompany/vemser/ContaTest.java
+++ modulo-04-1-junit/conta-corrente-4/src/test/java/br/com/dbccompany/vemser/ContaTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Conta Corrente - Teste")
-public class ContaCorrenteTest {
+@DisplayName("Conta - Teste")
+public class ContaTest {
     Cliente cliente1 = new Cliente("Cecília", "123456789");
+    ContaPoupanca contaPoupanca = new ContaPoupanca(cliente1, "21192140", "002", 1000.00);
+
     Cliente cliente2 = new Cliente("Madalena", "987654321");
     ContaCorrente contaCorrente = new ContaCorrente(cliente1,"14155432", "001", 100.00, 900.00);
     ContaCorrente contaCorrente2 = new ContaCorrente(cliente2,"14155433", "001", 100.00, 900.00);
+
+    ContaPagamento contaPagamento = new ContaPagamento(cliente1, contaCorrente.getNumeroConta(), contaCorrente.getAgencia(), contaCorrente.getSaldo());
 
     @Test
     void deveTestarSaqueContaCorrenteEVerificarSaldoComSucesso() {
@@ -23,6 +27,35 @@ public class ContaCorrenteTest {
         contaCorrente.sacar(1100.00);
         double valorObtido = contaCorrente.retornarSaldoComChequeEspecial();
         Assertions.assertEquals(1000.00, valorObtido);
+    }
+
+    @Test
+    void deveTestarSaqueContaPoupancaEVerificarSaldoComSucesso() {
+        contaPoupanca.creditarTaxa();
+        contaPoupanca.sacar(40.00);
+        double valorObtido = contaPoupanca.getSaldo();
+        Assertions.assertEquals(970.00, valorObtido);
+    }
+
+    @Test
+    void deveTestarSaqueContaPoupancaSemSaldo() {
+        contaPoupanca.sacar(1005.00);
+        double valorObtido = contaPoupanca.getSaldo();
+        Assertions.assertEquals(1000.00, valorObtido);
+    }
+
+    @Test
+    void deveTestarSaqueContaPagamentoEVerificarSaldoComSucesso(){
+        contaPagamento.sacar(40.75);
+        double valorObtido = contaPagamento.getSaldo();
+        Assertions.assertEquals(55.00, valorObtido);
+    }
+
+    @Test
+    void deveTestarSaqueContaPagamentoSemSaldo(){
+        contaPagamento.sacar(100.00);
+        double valorObtido = contaPagamento.getSaldo();
+        Assertions.assertEquals(100.00, valorObtido);
     }
 
     @Test
